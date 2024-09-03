8aded8b14557e6c332e0cad4fd2b1fbf14a3cd52
Qin-shihuang <0.0@owo.li>
2024-09-03 12:35:49 +0800

    Interactive

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main (main, readExpr) where
 
+import System.IO
 import System.Environment
 import Parser
 import Eval
@@ -12,7 +13,31 @@ readExpr input = case parse parseExpr "lisp" input of
   Left err -> throwError $ Parser err
   Right val -> return val
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr =  evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do 
+   result <- prompt
+   if pred result 
+      then return ()
+      else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
-main = do
-  args <- getArgs
-  putStrLn $ extractValue $ trapError $ fmap show $ readExpr (head args) >>= eval
+main = do args <- getArgs
+          case length args of
+               0 -> runRepl
+               1 -> evalAndPrint $ head args
+               _ -> putStrLn "Program takes only 0 or 1 argument"
